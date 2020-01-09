139e8bf0d1b9a08ccc02f56d91f1035ffb3beb91
Abhijit Menon <abhijit502@gmail.com>
2020-01-09 17:50:16 +0530

    Update rep.py

--- rep.py
+++ rep.py
@@ -6,7 +6,7 @@ csv_reader=csv.reader(csv_file, delimiter=',')
 a={}
 next(csv_reader)
 for lines in csv_reader:
-    key=str(lines[4])+'.'+str(lines[3])
+    key=str(lines[3])+'.'+str(lines[4])
     if key not in a:
         a.setdefault(key,[])
         a[key].append(int(lines[2]))
@@ -20,15 +20,17 @@ data = np.zeros( (200,200,3), dtype=np.uint8 )
 
 for key in a:
     a[key][0]=float(a[key][0])/(float(a[key][1]))/(-90)
-    print key,a[key]
     #data[int(key.split('.')[0]),int(key.split('.')[1])] = [int(a[key][0]*255),int(a[key][0]*255),int(a[key][0]*55)]
-    if a[key][0]>0.9:
-        data[int(key.split('.')[0]),int(key.split('.')[1])]=[255,255,255]
-    elif a[key][0]>0.8:
+    if a[key][0]>0.85:
         data[int(key.split('.')[0]),int(key.split('.')[1])]=[255,0,0]
-    elif a[key][0]>0.7:
+    elif a[key][0]>0.80:
+        data[int(key.split('.')[0]),int(key.split('.')[1])]=[255,255,0]
+    elif a[key][0]>0.75:
         data[int(key.split('.')[0]),int(key.split('.')[1])]=[0,255,0]
-    elif a[key][0]>0.6:
+    elif a[key][0]>0.70:
+        data[int(key.split('.')[0]),int(key.split('.')[1])]=[0,255,255]
+    elif a[key][0]>0.65:
         data[int(key.split('.')[0]),int(key.split('.')[1])]=[0,0,255]
+
 img = smp.toimage( data )       # Create a PIL image
 img.show()
