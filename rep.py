67681f6ab5940b83a645bbede04b9b63ee4c1720
Abhijit Menon <abhijit502@gmail.com>
2020-01-09 15:30:13 +0530

    Update rep.py

--- rep.py
+++ rep.py
@@ -1,32 +1,27 @@
 import csv
+import numpy as np
+import scipy.misc as smp
+csv_file=open('data1.csv','r')
+csv_reader=csv.reader(csv_file, delimiter=',')
+a={}
+next(csv_reader)
+for lines in csv_reader:
+    key=str(lines[4])+'.'+str(lines[3])
+    if key not in a:
+        a.setdefault(key,[])
+        a[key].append(int(lines[2]))
+        a[key].append(1)
+    if key in a:
+        a[key][0]=a[key][0]+int(lines[2])
+        a[key][1]=a[key][1]+1
 
+# Create a 200x200x3 array of 8 bit unsigned integers
+data = np.zeros( (200,200,3), dtype=np.uint8 )
 
-count_matrix=[[0 for x in range(90,170)] for y in range(50,170)]
+for key in a:
+    a[key][0]=float(a[key][0])/(float(a[key][1]))/(-90)
+    print key,a[key]
+    data[int(key.split('.')[0]),int(key.split('.')[1])] = [int(a[key][0]*255),int(a[key][0]*255),int(a[key][0]*255)]
 
-plots={}
-for points_y in range(90,170):
-	plots.setdefault(points_y,[])
-	for points_x in range(50,170):
-		plots[points_y].append(0)
-#for x in plots:
-	#print x,plots[x]
-
-#plots.setdefaults()
-with open('/root/weyefeye/data/data1.csv') as csv_file:
-	csv_read=csv.reader(csv_file,delimiter=',')
-	for lines in csv_read:
-		try:
-			print lines[2],lines[3],lines[4]
-			plots[int(lines[3])][int(lines[4])-50]=int(lines[2])
-			count_matrix[int(lines[3])][int(lines[4])]+=1				
-			#print plots[int(lines[3])][int(lines[4])-50]
-		except:
-			continue
-	for y in plots:
-		print y,plots[y],"\n"
-		print "counts", len(count_matrix),len(plots)
-		
-		print "\n"
-	#print plots[113],plots[113][93]
-	for point in range(0,170):
-		print point, count_matrix[point]	
+img = smp.toimage( data )       # Create a PIL image
+img.show()
